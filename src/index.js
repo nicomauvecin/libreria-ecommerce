64316c19d1c4899772fd3413d6f6dfec0a4e5a5a
Nicolás Mauvecin <80804441+nicomauvecin@users.noreply.github.com>
2021-09-19 00:59:11 -0300

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-import { renderHTML } from './ui.js';
+import { getCategories, getData } from './api.js';
+import { loading, renderHTML, renderProducts, renderMenu } from './ui.js';
 
-function init() {
+const app = document.querySelector('#app');
+const initialCategory = 'programacion';
+const initialLimit = 0;
+const finalLimit = 32;
+
+async function init() {
   renderHTML();
+
   new Splide('#splide', {
     perPage: 3,
     rewind: true,
   }).mount();
-}
 
+  loading();
+  await renderMenu(getCategories);
+  await renderProducts(initialCategory, initialLimit, finalLimit, getData);
+}
 init();
