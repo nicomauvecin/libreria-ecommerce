4a994a557a4a0d1dcfe9bb7941f8678b2f443656
Nicolás Mauvecin <80804441+nicomauvecin@users.noreply.github.com>
2021-10-01 11:43:08 -0300

    update ui.js

--- src/ui.js
+++ src/ui.js
@@ -5,13 +5,13 @@ export const renderHTML = () => {
   <header class="header">
   <div class="notification">ENVIOS GRATIS A TODO EL PAIS DESDE $4000.</div>
   <nav class="navbar">
-    <div class="toggle-container">☰</div>
+    <div class="toggle-container"><i class="fas fa-user"></i></div>
     <div class="brand-container">
       <h1>CEMENTERIO DE LIBROS</h1>
     </div>
     <div class="cart-menu" id="cart" data-toggle="modal" data-target="#exampleModalCenter">
       <i class="fas fa-shopping-cart"></i>
-      <span id="cart">1</span>
+      <span id="cart-items">0</span>
     </div>
   </nav>
 </header>
@@ -31,18 +31,20 @@ export const renderHTML = () => {
 <div class="wrapper">
 <div class="menu-container">
     <h2>Categorias</h2>
-    <ul id="menu"></ul>
+    <ul id="menu">
+      <h2 class="loading">Cargando...</h2>
+    </ul>
 </div>
 <div class="products-container" id="container">
 </div>
     `;
 };
 
+let itemsCart = 0;
+
 export function loading() {
   const container = document.querySelector('#container');
-  const menu = document.querySelector('#menu');
-
-  menu.innerHTML = `<h2 class="loading">Cargando...</h2>`;
+  container.classList.remove('active');
 
   container.innerHTML = `
     <h2 class="loading">Cargando...</h2>
@@ -56,14 +58,14 @@ export async function renderMenu(cb) {
   respuesta.forEach((element) => {
     menu.innerHTML += `
       <li class="menu-item">
-      <span data-id="${element.category_id}">${element.name}</span>
+      <span id="item" data-id="${element.category_id}">${element.name}</span>
       </li>
       `;
   });
 }
 
-export async function renderProducts(categoria, inicial, final, cb) {
-  const respuesta = await cb(categoria, inicial, final);
+export async function renderProducts(categoria, cb) {
+  const respuesta = await cb(categoria);
   const container = document.querySelector('#container');
   container.innerHTML = '';
   container.classList.add('active');
@@ -76,7 +78,7 @@ export async function renderProducts(categoria, inicial, final, cb) {
         <img src="../static/stars.png">
         <h4>$<span id="price">9.99</span></h4>
         <button class="btn btn-principal" id="btn-info" data-toggle="modal" data-target="#exampleModalCenter">Ver Info</button>
-        <button class="btn btn-principal">Agregar al carrito</button>
+        <button class="btn btn-principal" id="btn-cart">Agregar al carrito</button>
       </div>
     </div>
     `;
@@ -84,54 +86,145 @@ export async function renderProducts(categoria, inicial, final, cb) {
 }
 
 export function clickHandler(
-  cbAPI,
+  cbAPIData,
+  cbAPIBook,
   cbRenderBookHTML,
   cbRenderBookData,
-  cbRenderCartHTML
+  cbRenderCartHTML,
+  cbCart,
+  cbClearCart,
+  cbClearItem
+) {
+  productsHandler(cbAPIBook, cbRenderBookHTML, cbRenderBookData, cbCart);
+  menuHandler(cbAPIData);
+  cartHandler(cbRenderCartHTML, cbCart);
+  modalHandler(cbRenderCartHTML, cbCart, cbClearItem, cbClearCart);
+}
+
+function productsHandler(
+  cbAPIBook,
+  cbRenderBookHTML,
+  cbRenderBookData,
+  cbCart
 ) {
   const productsContainer = document.querySelector('.products-container');
   productsContainer.onclick = async (e) => {
-    if (e.target.id != 'btn-info') {
-      return;
+    if (e.target.id === 'btn-info') {
+      cbRenderBookHTML();
+      const bookID = e.target.parentNode.parentNode.dataset.id;
+      const response = await cbAPIBook(bookID);
+      await cbRenderBookData(response);
+    } else if (e.target.id === 'btn-cart') {
+      const selectedBook = e.target.parentNode.parentNode;
+      const bookID = selectedBook.dataset.id;
+      const bookIMG = selectedBook.querySelector('img').src;
+      const bookTITLE = selectedBook.querySelector('h3').textContent;
+      cbCart(bookID, bookIMG, bookTITLE);
+      increaseCartItems();
     }
-    cbRenderBookHTML();
-    const bookID = e.target.parentNode.parentNode.dataset.id;
-    const response = await cbAPI(bookID);
-    await cbRenderBookData(response);
+  };
+}
+
+function menuHandler(cbAPIData) {
+  const menu = document.querySelector('#menu');
+  menu.onclick = (e) => {
+    if (e.target.id === 'item') {
+      const selectedCategory = e.target.dataset.id;
+      loading();
+      renderProducts(selectedCategory, cbAPIData);
+    }
+  };
+}
+
+function cartHandler(cbRenderCartHTML, cbCart) {
+  const btnCart = document.querySelector('#cart');
+  btnCart.onclick = () => {
+    cbRenderCartHTML(cbCart(null));
+  };
+}
+
+function modalHandler(cbRenderCartHTML, cbCart, cbClearItem, cbClearCart) {
+  modalBody.onclick = (e) => {
+    if (e.target.id === 'clear-cart-item') {
+      const selectedItem = e.target.parentNode.parentNode;
+      const quantityItems = Number(
+        selectedItem.querySelector('#book-quantity').innerText
+      );
+      console.log(quantityItems);
+      const itemID = selectedItem.dataset.id;
+      cbRenderCartHTML(cbClearItem(itemID));
+      decreaseCardItems(quantityItems);
+    }
   };
 
-  const cart = document.querySelector('#cart');
-  cart.onclick = () => {
-    console.log('asd');
-    cbRenderCartHTML();
+  modalFooter.onclick = (e) => {
+    if (e.target.id === 'buy-cart-modal') {
+      const selectedBook = e.target.parentNode.parentNode;
+      const bookID = selectedBook.dataset.id;
+      const bookIMG = selectedBook.querySelector('img').src;
+      const bookTITLE = selectedBook.querySelector('h3').textContent;
+      cbCart(bookID, bookIMG, bookTITLE);
+    }
+    if (e.target.id === 'clear-cart') {
+      cbRenderCartHTML(cbClearCart());
+      decreaseCardItems();
+    }
   };
+}
+
+function increaseCartItems() {
+  itemsCart++;
+  const cartItems = document.querySelector('#cart-items');
+  cartItems.innerText = itemsCart;
+}
+
+function decreaseCardItems(quantity = itemsCart) {
+  itemsCart = itemsCart - quantity;
+  const cartItems = document.querySelector('#cart-items');
+  cartItems.innerText = itemsCart;
 }
 
 const modalBody = document.querySelector('.modal-body');
 const modalFooter = document.querySelector('.modal-footer');
 
-export function renderModalCart() {
-  modalBody.innerHTML = `
-  <div class="modal-info">
-    <h3 id="modal-book-title"></h4>
-    <h5>$<span id="modal-book-price">9.99</span></h5>
-  </div>
-  <div class="modal-img">
-    <img src="" id="modal-book-img">
-  </div> 
-  `;
+export function renderModalCart(cart) {
+  modalBody.innerHTML = '';
+  modalBody.className = 'modal-body-cart';
+  if (cart.length === 0) {
+    modalBody.innerHTML = `<h3>El carrito está vacío</h3>`;
+    modalFooter.innerHTML = '';
+    return;
+  }
 
+  cart.forEach((element) => {
+    modalBody.innerHTML += `
+    <div class="cart-item" data-id="${element.id}">
+      <div class="modal-info">
+        <h3 id="modal-book-title">${element.title}</h4>
+        <h5>$<span id="modal-book-price">9.99</span> - Cantidad: <span id="book-quantity">${element.items}</span></h5>
+        <button type="button" class="btn btn-principal" id="clear-cart-item">
+        Quitar del carrito
+        </button>
+      </div>
+      <div class="modal-img-cart">
+        <img src="${element.src}" id="modal-book-img">
+      </div>
+    </div>
+    `;
+  });
   modalFooter.innerHTML = `
-  <button type="button" class="btn btn-principal">
+  <h4>Total: ${cart.length * 9.99}</h4>
+  <button type="button" class="btn btn-principal" id="clear-cart">
     Vaciar Carrito
   </button>
-  <button type="button" class="btn btn-secundario" data-dismiss="modal">
+  <button type="button" class="btn btn-secundario" id="buy-cart">
     Comprar
   </button>
   `;
 }
 
 export function renderModalBook() {
+  modalBody.className = 'modal-body';
   modalBody.innerHTML = `
   <div class="modal-info">
     <h3 id="modal-book-title">Cargando...</h4>
@@ -145,7 +238,7 @@ export function renderModalBook() {
   `;
 
   modalFooter.innerHTML = `
-  <button type="button" class="btn btn-principal">
+  <button type="button" class="btn btn-principal" id="buy-cart-modal">
     Agregar al carrito
   </button>
   <button type="button" class="btn btn-secundario" data-dismiss="modal">
@@ -160,6 +253,8 @@ export function renderBookData(response) {
   const modalDescription = modalBody.querySelector('#modal-book-content');
   const modalImage = modalBody.querySelector('#modal-book-img');
 
+  const modalContent = document.querySelector('.modal-content');
+  modalContent.dataset.id = response[0].ID;
   modalTitle.innerText = response[0].title;
   modalAuthor.innerText = response[0].author;
   modalDescription.innerText = response[0].content_short;
